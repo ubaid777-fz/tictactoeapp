9a09d23ebd8f8724c69fbd26b7ce90c1ae064c28
Ubaid <ubaidmohammad2007@gmail.com>
2026-04-22 22:27:59 +0530

    UC3: Allow user to enter slot number (1-9)

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,28 +1,19 @@
-import java.util.Random;
+import java.util.Scanner;
 
 public class TicTacToe {
 
+    // Method to take user input
+    public static int getUserInput() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a slot number (1-9): ");
+        int slot = scanner.nextInt();
+        return slot;
+    }
+
     public static void main(String[] args) {
 
-        Random random = new Random();
-        int toss = random.nextInt(2);
+        int slot = getUserInput();
 
-        char player1Symbol, player2Symbol;
-        int currentPlayer;
-
-        if (toss == 0) {
-            currentPlayer = 1;
-            player1Symbol = 'X';
-            player2Symbol = 'O';
-        } else {
-            currentPlayer = 2;
-            player2Symbol = 'X';
-            player1Symbol = 'O';
-        }
-
-        System.out.println("Toss Result:");
-        System.out.println("Player " + currentPlayer + " starts");
-        System.out.println("Player 1: " + player1Symbol);
-        System.out.println("Player 2: " + player2Symbol);
+        System.out.println("You selected slot: " + slot);
     }
 }
