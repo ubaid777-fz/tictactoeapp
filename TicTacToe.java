a6b77c7ac9b4bf93af011c132c1e28d0db4e50cf
Ubaid <ubaidmohammad2007@gmail.com>
2026-04-22 21:58:34 +0530

    UC1: Display empty board

---
+++ TicTacToe.java
@@ -0,0 +1,23 @@
+public class TicTacToe {
+
+    public static void main(String[] args) {
+
+        char[][] board = new char[3][3];
+
+        // Initialize board
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = '-';
+            }
+        }
+
+        // Display board
+        System.out.println("Initial Board:");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
