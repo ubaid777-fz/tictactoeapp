a63f7ca0a69aa308f0e394fd29d98591d03462ef
Ubaid <ubaidmohammad2007@gmail.com>
2026-04-22 22:15:46 +0530

    UC2: Toss to decide first player and assign symbols

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,23 +1,28 @@
+import java.util.Random;
+
 public class TicTacToe {
 
     public static void main(String[] args) {
 
-        char[][] board = new char[3][3];
+        Random random = new Random();
+        int toss = random.nextInt(2);
 
-        // Initialize board
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                board[i][j] = '-';
-            }
+        char player1Symbol, player2Symbol;
+        int currentPlayer;
+
+        if (toss == 0) {
+            currentPlayer = 1;
+            player1Symbol = 'X';
+            player2Symbol = 'O';
+        } else {
+            currentPlayer = 2;
+            player2Symbol = 'X';
+            player1Symbol = 'O';
         }
 
-        // Display board
-        System.out.println("Initial Board:");
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                System.out.print(board[i][j] + " ");
-            }
-            System.out.println();
-        }
+        System.out.println("Toss Result:");
+        System.out.println("Player " + currentPlayer + " starts");
+        System.out.println("Player 1: " + player1Symbol);
+        System.out.println("Player 2: " + player2Symbol);
     }
 }
